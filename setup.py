deec3a3338a2e62872a638e7c9fe1a496f18b5b6
DRONE <dronyboi>
2024-03-21 10:55:18 +0100

    redundant changes

--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@ setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+         ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ascend',
@@ -15,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'suas24_localization_visualization = suas24_localization_visualization.suas24_localization_visualization'
+            'suas24_localization_visualization = suas24_localization_visualization.suas24_localization_visualization:main'
         ],
     },
 )
