b7ba71e87dc41b6c9b093c02427848e5b9040171
DRONE <dronyboi>
2024-03-26 14:17:28 +0100

    feat: add heatmap visualization

--- suas24_localization_visualization/suas24_localization_visualization.py
+++ suas24_localization_visualization/suas24_localization_visualization.py
@@ -1,76 +1,93 @@
 import rclpy
 from rclpy.node import Node
-
+import cv2
 from std_msgs.msg import Int32MultiArray
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import draw
 import numpy as np
+from suas24_interfaces.msg import VisualizationImgs
 
 
+#heatmap filter variables
+kernel_size = round(5 / 0.01)
+kernel = cv2.getGaussianKernel(kernel_size + (1 - kernel_size % 2), 0, cv2.CV_32F)
+
+
+def sensormsg_to_numpy(msg):
+    """
+        Expects msg.data to be of type uint8
+    """
+    if msg.height <= 1:
+        return (False, False)
+
+    img = np.frombuffer(msg.data, dtype=np.uint8).reshape((msg.height, msg.width)) #simply a grey scale image
+    return (True, img)
+
+def create_heatmap(img):
+    if len(img.shape) > 2:
+        img = gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
+        print(f"grayscale shape: {img.shape}")
+    
+    img = img.astype(np.float32)
+
+    print(f"SUM: {np.sum(img)}")
+    heatmap = cv2.sepFilter2D(img, -1, kernel, kernel)
+    print(f"SUM AFTER HEATMAP: {np.sum(heatmap)}")
+    return heatmap
 
 class LocalizationVisualization(Node):
 
     def __init__(self):
         super().__init__('LocalizationVisualization')
         self.subscription = self.create_subscription(
-            Int32MultiArray,
-            '/viz/points',
+            VisualizationImgs,
+            '/viz/heatmap',
             self.listener_callback,
             10)
         self.subscription
+        self.n_objects = 5 
+
 
         plt.ion() #enables interatctive mode
         plt.show()
-        array = np.array([[255, 0],
-                         [0, 255]], dtype=np.uint8)
- 
-        self.n_objects = 5
-        self.drop_points = np.zeros((self.n_objects, 300, 300))
 
+        self.drop_points = np.zeros((self.n_objects, 20, 20))
+        
         self.fig, self.axx = plt.subplots(3, 2)
         self.axx[0, 0].imshow(self.drop_points[0])
         
 
-        #plt.imshow(self.drop_points[0])
         plt.draw()
         plt.pause(0.01)
 
 
     def listener_callback(self, msg):
-        data = msg.data
-        index = data[0]
-        x = data[1]
-        y = data[2]
-        #z = data[3]
-        conf = data[4]
 
-        axx_row = index // 2
-        axx_col = 0 if index % 2 == 0 else 1
+        images = msg.images
+        self.get_logger().info("Heigth: %s" % images[0].height)
+        for idx, object in enumerate(images):
+            #self.get_logger().info("Received image: %s" % image)
+            img = sensormsg_to_numpy(object) 
 
-        #intervals for the points such that the point becomes larger (visible)
-        x_min = max(0, x - 5) 
-        x_max = min(300, x + 5)
-        y_min = max(0, y - 5)
-        y_max = min(300, y + 5)
+            if img[0]:
+                print(img[1].shape)
 
-        #don't ask...
-        x_min = min(x_min, x_max - 1)
-        x_max = max(x_max, x_min + 1)
-        y_min = min(y_min, y_max - 1)
-        y_max = max(y_max, y_min + 1)
+                heatmap = create_heatmap(img[1])
 
-        self.drop_points[index][x_min:x_max, y_min:y_max] = 255
+                axx_row = idx // 2
+                axx_col = 0 if idx % 2 == 0 else 1
 
-        self.axx[axx_row, axx_col].imshow(self.drop_points[index])
+                self.axx[axx_row, axx_col].imshow(heatmap)
+                plt.draw()
+                plt.pause(0.01)
 
+                if idx == 1:
+                    cv2.imwrite("/home/ascend/repos/mono24/normal.png", img[1])
+                    cv2.imwrite("/home/ascend/repos/mono24/heatmap.png", heatmap)
+                
 
-        #plt.imshow(array)
-        plt.draw()
-        plt.pause(0.01)
+        self.get_logger().info("Drawed!")
 
-        self.get_logger().info('Logged : "%s"' % data)
-        self.get_logger().info(f"sum: {np.sum(self.drop_points[index])}")
-        
 
 def main(args=None):
     rclpy.init(args=args)
@@ -79,3 +96,115 @@ def main(args=None):
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###############################################################################################################
+# class LocalizationVisualization(Node):
+
+#     def __init__(self):
+#         super().__init__('LocalizationVisualization')
+#         self.subscription = self.create_subscription(
+#             Int32MultiArray,
+#             '/viz/points',
+#             self.listener_callback,
+#             10)
+#         self.subscription
+        
+        
+#         self.img_height = 400
+#         self.img_width = 400
+#         self.n_objects = 5
+#         self.magic_offset = 70 #burde være
+#         self.min_x = float('inf')
+#         self.max_x = float('-inf')
+#         self.min_y = float('inf')
+#         self.max_y = float('-inf')
+#         #self.bag_start_enu = np.array([10, 14, 25]) * 10 #local enu coordinates at the time the bag starts, scaled same as the message coordinates
+
+#         plt.ion() #enables interatctive mode
+#         plt.show()
+ 
+#         self.drop_points = np.zeros((self.n_objects, self.img_height, self.img_width))
+#         self.drop_points[self.img_height//2-10 : self.img_height//2+10, self.img_width//2-10 : self.img_width//2+10] = 150 #display origin
+
+#         self.fig, self.axx = plt.subplots(3, 2)
+#         self.axx[0, 0].imshow(self.drop_points[0])
+        
+
+#         #plt.imshow(self.drop_points[0])
+#         plt.draw()
+#         plt.pause(0.01)
+
+
+#     def listener_callback(self, msg):
+#         #x = rows
+#         #y = cols
+
+#         data = msg.data
+#         index = data[0]
+#         x = data[1] + self.img_height // 2 #add offsets to adjust origin
+#         y = data[2] - self.magic_offset
+#         self.get_logger().info(f"Log here: {x, y}")
+#         #z = data[3] #trivial
+#         conf = data[4]
+
+#         axx_row = index // 2
+#         axx_col = 0 if index % 2 == 0 else 1
+
+#         #intervals for the points such that the point becomes larger (visible)
+#         padding = 7
+#         x_min = max(0, x - padding) 
+#         x_max = min(self.img_height, x + padding)
+#         y_min = max(0, y - padding)
+#         y_max = min(self.img_width, y + padding)
+
+#         self.drop_points[index][x_min:x_max, y_min:y_max] = 255
+
+#         self.axx[axx_row, axx_col].imshow(self.drop_points[index])
+#         #self.get_logger().info(f"{x_min, x_max, y_min, y_max}")
+
+#         plt.draw()
+#         plt.pause(0.01)
+
+#         self.get_logger().info('Logged : "%s"' % data)
+#         self.get_logger().info(f"sum: {np.sum(self.drop_points[index])}")
+
+#     # def adjust_image_size(self):
+#     #     new_height = 
+#     #     new_width = 
+        
+
+# def main(args=None):
+#     rclpy.init(args=args)
+
+#     node = LocalizationVisualization()
+#     rclpy.spin(node)
+#     node.destroy_node()
+#     rclpy.shutdown()
